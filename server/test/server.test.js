1bdc20b3b89727f09d7a57ea6fe5ede719f1d8e9
bmayer4 <bmayer55@yahoo.com>
2017-10-25 22:04:27 -0400

    add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -196,7 +196,7 @@ describe('GET /users/me', () => {
   it('should return user if authenticated', (done) => {
     request(app)
     .get('/users/me')
-    .set('x-auth', users[0].tokens[0].token)
+    .set('x-auth', users[0].tokens[0].token)  //.set sets header
     .expect(200)
     .expect((res) => {
       expect(res.body._id).toBe(users[0]._id.toHexString());
@@ -314,6 +314,29 @@ describe('POST /users/login', () => {
     });
   });
 
+});
+
+});
+
+
+describe('DELETE /users/me/token', () => {
+
+it('should remove auth token on logout', (done) => {
+  request(app)
+  .delete('/users/me/token')
+  .set('x-auth', users[0].tokens[0].token)
+  .expect(200)
+  .end((err, res) => {
+    if (err) {
+      return done(err);
+    }
+    User.findById(users[0]._id).then((user) => {
+      expect(user.tokens.length).toBe(0);
+      done();
+    }).catch((e) => {
+      done(e);
+    });
+  });
 });
 
 });
