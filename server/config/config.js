03e7914a81315d5d93f9cef53622dd5b3fb1f611
bmayer4 <bmayer55@yahoo.com>
2017-10-27 20:34:12 -0400

    update config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,41 @@
 var env = process.env.NODE_ENV || 'development';  //first one set on production and test, second on dev
-console.log('env *****', env);
+console.log('env *****', env);   //set to development by default
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  //when you require a json file, it automatically parses it into a javascript object, no need for JSON.parse
+  var config = require('./config.json');
+  var envConfig = config[env]; //when you use variable to access a property, you have to use bracket notation
+  console.log(envConfig);
+
+  console.log(envConfig['PORT']);  //3000
+    console.log(envConfig['MONGODB_URI']);  //mongodb://localhost:27017/TodoApp
+    console.log(envConfig['JWT_SECRET']);  //de3ud3hd340hd30io4
+  //Object.keys() takes an object like env.config, returns the object keys as an array
+  Object.keys(envConfig).forEach((key) => {  //[PORT, MONGODB_URI, JWT_SECRET]
+    process.env[key] = envConfig[key]
+  });
+
 }
+
+//for heroku, set custom env variable
+//heroku config:set MYNAME=Brett
+
+//gets var
+//heroku config:get MYNAME
+
+//unsets
+//heroku config:unset MYNAME
+
+//NOW I have to set this variable in heroku since we  use it in the app 
+//heroku config:set JWT_SECRET=343efijrio544h
+
+
+
+//moved this to config.json for security reasons
+// if (env === 'development') {
+//   process.env.PORT = 3000;   //port PORT and MONGODB_URI are both set by heroku on production
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === "test") {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -51,7 +51,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {  //arrow func doesn't bind to this keyword, this stores indiv doc
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id, access: access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id, access: access}, process.env.JWT_SECRET).toString();
 
   user.tokens.push({   //_id is generated here by mongodb
     access: access,
@@ -82,7 +82,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;  //undedined because jwt.verify throws error if something goes wrong
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     // return new Promise((resolve, reject) => {
     //   reject();
